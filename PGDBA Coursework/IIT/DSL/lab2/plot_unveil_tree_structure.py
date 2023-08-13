846b79ba871d574daef97ae813480e288ada2295
Utkarsh Yadav <utkarshvns94@gmail.com>
2023-08-13 14:08:51 +0530

    Add files via upload

---
+++ PGDBA Coursework/IIT/DSL/lab2/plot_unveil_tree_structure.py
@@ -0,0 +1,81 @@
+"""
+=========================================
+Understanding the decision tree structure
+=========================================
+
+The decision tree structure can be analysed to gain further insight on the
+relation between the features and the target to predict. In this example, we
+show how to retrieve:
+
+- the binary tree structure;
+- the depth of each node and whether or not it's a leaf;
+- the nodes that were reached by a sample using the ``decision_path`` method;
+- the leaf that was reached by a sample using the apply method;
+- the rules that were used to predict a sample;
+- the decision path shared by a group of samples.
+
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import numpy as np
+##############################################################################
+
+
+##############################################################################
+#
+
+n_nodes = clf.tree_.node_count
+children_left = clf.tree_.children_left
+children_right = clf.tree_.children_right
+feature = clf.tree_.feature
+threshold = clf.tree_.threshold
+
+node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+stack = [(0, 0)]  # start with the root node id (0) and its depth (0)
+while len(stack) > 0:
+    # `pop` ensures each node is only visited once
+    node_id, depth = stack.pop()
+    node_depth[node_id] = depth
+
+    # If the left and right child of a node is not the same we have a split
+    # node
+    is_split_node = children_left[node_id] != children_right[node_id]
+    # If a split node, append left and right children and depth to `stack`
+    # so we can loop through them
+    if is_split_node:
+        stack.append((children_left[node_id], depth + 1))
+        stack.append((children_right[node_id], depth + 1))
+    else:
+        is_leaves[node_id] = True
+
+print("The binary tree structure has {n} nodes and has "
+      "the following tree structure:\n".format(n=n_nodes))
+for i in range(n_nodes):
+    if is_leaves[i]:
+        print("{space}node={node} is a leaf node.".format(
+            space=node_depth[i] * "\t", node=i))
+    else:
+        print("{space}node={node} is a split node: "
+              "go to node {left} if X[:, {feature}] <= {threshold} "
+              "else to node {right}.".format(
+                  space=node_depth[i] * "\t",
+                  node=i,
+                  left=children_left[i],
+                  feature=feature[i],
+                  threshold=threshold[i],
+                  right=children_right[i]))
+
+##############################################################################
+# We can compare the above output to the plot of the decision tree.
+
+tree.plot_tree(clf)
+plt.show()
+
+##############################################################################
+# 
